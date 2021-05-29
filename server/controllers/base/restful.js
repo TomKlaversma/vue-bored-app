f8397612f5754a5854d89a6114482942492de33f
Tom <tomklaversma@gmail.com>
2021-05-29 12:11:28 +0200

    Added Express server backend

---
+++ server/controllers/base/restful.js
@@ -0,0 +1,15 @@
+const HttpException = require('../../exceptions/http');
+
+module.exports = () => ({
+  respondSuccess(res, body) {
+    return res.send({
+      status: 'ok',
+      statusCode: 200,
+      body,
+    });
+  },
+  handleError(error, status, cb) {
+    const exception = HttpException(status, error);
+    return cb(exception);
+  },
+});
---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const enforce = require('express-sslify');
+const api = require('./api');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+const VUE_APP_DIR = path.join(__dirname, '..', '/dist');
+
+if (process.env.NODE_ENV === 'production') {
+  // Use SSL on Heroku
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+  // Serve build VUE app
+  app.use(express.static(VUE_APP_DIR));
+}
+
+// add API
+app.use('/api', api);
+
+app.use((err, req, res) => {
+  console.error(err.error.stack);
+  const status = err.status || 500;
+  return res.status(status).send('Something went wrong while processing your request!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
